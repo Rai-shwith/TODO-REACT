bee7f893334f1a5c93bf111c6107b84d842f18d3
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-11-12 18:08:52 +0530

    Todo complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,38 @@ import ToDo from "./components/ToDo";
 import ToDoContainer from "./components/ToDoContainer";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+
 function App() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
+  const [showFinished, setShowFinished] = useState(true);
 
-  const handleAdd = (e) => {
-    if (toDo.trim() && e.key == "Enter") {
+const toggleShowFinished = () => { 
+  setShowFinished(!showFinished);
+ }
+
+
+const saveToLocalStorage=()=>{
+  setTimeout(() => {
+    localStorage.setItem('todos',JSON.stringify(toDos))
+  }, 300);
+  console.log("Todos Saved !")
+}
+useEffect(() => {
+  const localTodos = JSON.parse(localStorage.getItem('todos'));
+  localTodos && setToDos(localTodos);
+}, [])
+useEffect(() => {
+  saveToLocalStorage()
+}, [toDos])
+
+
+
+  const handleAdd = (e,fromBtn) => {
+    if (toDo.trim() && (e.key == "Enter" || fromBtn)) {
       setToDos([...toDos, { id: uuidv4(),toDo: toDo, isCompleted: false }]);
       setToDo("");
+      saveToLocalStorage()
     } 
   };
 
@@ -33,17 +57,25 @@ function App() {
           className="appearance-none w-4/5 focus:ring-2 focus:ring-offset-2 outline-none"
         />
         <h3
-          onClick={handleAdd}
+          onClick={(e)=>handleAdd(e,true)}
           className="p-2 bg-slate-400 hover:scale-110 transition-transform ease-in-out rounded-md cursor-pointer"
         >
-          Add
+          Save
         </h3>
       </div>
+      <div className="flex gap-5 justify-center">
+        <input type="checkbox" onClick={toggleShowFinished} name="" id=""  defaultChecked={showFinished} className=""/> Show Finished
+        </div>
+        <div className="h-0.5 w-3/4 self-center bg-black opacity-30 my-1"></div>
       <ToDoContainer>
+        {toDos.length==0&& <div className="text-center text-3xl font-semibold">No Todos</div>}
       {toDos.map((todo) => (
-          <ToDo  key={todo.id} id={todo.id} todoDetails={todo} todo={todo.toDo}
+          <ToDo hidden = {!showFinished && todo.isCompleted}
+          key={todo.id} id={todo.id} todoDetails={todo} todo={todo.toDo}
           todos = {toDos}
           setToDos = {setToDos}
+          setToDo = {setToDo}
+          saveToLocalStorage = {saveToLocalStorage}
           ></ToDo>
         ))}
         </ToDoContainer>
